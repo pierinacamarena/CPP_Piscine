dbb72ee86b3ac4d4f5e6dd13df74a31d9791d073
Pierina alessandra CAMARENA PUCUHUANCA <pcamaren@e1r8p10.clusters.42paris.fr>
2022-08-31 20:24:11 +0200

    added functions to the Zombie class

--- day01/ex00/Zombie.hpp
+++ day01/ex00/Zombie.hpp
@@ -6,7 +6,29 @@
 /*   By: pcamaren <pcamaren@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/31 20:08:13 by pcamaren          #+#    #+#             */
-/*   Updated: 2022/08/31 20:08:14 by pcamaren         ###   ########.fr       */
+/*   Updated: 2022/08/31 20:19:56 by pcamaren         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef ZOMBIE_H
+#define ZOMBIE_H
+
+#include <iostream>
+#include <string>
+
+class Zombie{
+	public:
+
+		Zombie(void);
+		~Zombie(void);
+	
+		void		announce(void);
+		void		setZombieName(const std::string name);
+		std::string	getZombieName(void) const;
+		
+	private:
+	
+		std::string _name;
+};
+
+#endif
