8cd0e64355cee277fde81fb80a353047818e92a8
Pierina Camarena <pierinaacam@gmail.com>
2022-08-31 13:48:57 +0200

    implemented constructor and destructor

--- day0/ex02/Account.cpp
+++ day0/ex02/Account.cpp
@@ -6,14 +6,33 @@
 /*   By: pierina <pierina@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/31 10:41:05 by pierina           #+#    #+#             */
-/*   Updated: 2022/08/31 10:42:15 by pierina          ###   ########.fr       */
+/*   Updated: 2022/08/31 13:47:29 by pierina          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
 #include "Account.hpp"
 
-Account::Account(void){
+int Account::_nbAccounts = 0;
+int Account::_totalAmount = 0;
+int Account::_totalNbDeposits = 0;
+int Account::_totalNbWithdrawals = 0;
+
+Account::Account(int initial_deposit)
+{
+	this->_amount = initial_deposit;
+	this->_accountIndex = Account::_nbAccounts;
+	this->_nbAccounts++;
+	this->_totalAmount += this->_amount;
+	Account::_displayTimestamp();
+	std::cout << "index:" << this->_accountIndex << ";";
+	std::cout << "amount:" << this->_amount << ";";
+	std::cout << "created" << std::endl;
 }
 
 Account::~Account(void){
+	Account::_displayTimestamp();
+	std::cout << "index:" << this->_accountIndex << ";";
+	std::cout << "amount:" << this->_amount << ";";
+	std::cout << "closed" << std::endl;
 }
